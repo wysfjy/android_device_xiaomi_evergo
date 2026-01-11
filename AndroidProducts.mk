e7ae29028524d6f262d3a02c77e6ea3aba0595ee
wysfjy <769643541@qq.com>
2026-01-11 10:50:11 +0800

    Fix for android-14.1

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -9,6 +9,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/omni_evergo.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_evergo-user \
-    omni_evergo-userdebug \
-    omni_evergo-eng
+    omni_evergo-ap2a-user \
+    omni_evergo-ap2a-userdebug \
+    omni_evergo-ap2a-eng
--- device.mk
+++ device.mk
@@ -18,8 +18,8 @@ PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
     android.hardware.boot@1.0-service
 
-PRODUCT_PACKAGES += \
-    bootctrl.mt6833
+#PRODUCT_PACKAGES += \
+#    bootctrl.mt6833
 
 PRODUCT_STATIC_BOOT_CONTROL_HAL := \
     bootctrl.mt6833 \
